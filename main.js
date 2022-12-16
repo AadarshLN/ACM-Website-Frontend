abbfc8b2cf8fb84a5cf8b0917e0bb23d8be35e1c
AadarshLN <ln.aadarsh03@gmail.com>
2022-12-16 20:32:32 +0530

    first set of changes

---
+++ main.js
@@ -0,0 +1,14 @@
+function darkMode()
+{
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    
+    
+}
+let inputOne=document.getElementById("first-name");
+let inputTwo=document.getElementById("last-name");
+let inputThree=document.getElementById("why");
+var submit=document.getElementById("submit");
+
+
+    
